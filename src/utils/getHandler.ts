e7ba2b29e71b2dfd89628f44a3ae3b6295f85c08
maki <marketa@vlach.io>
2023-01-06 22:43:28 +0100

    * Add ability to completely choose handlerRoot * Ability to use kebab-case resource naming style

--- src/utils/getHandler.ts
+++ src/utils/getHandler.ts
@@ -2,12 +2,13 @@ import { isPlural } from 'pluralize';
 import { QueryMethod, GetHandlerParams, GetHandlerModuleParams } from '../types';
 import { getEntityNameFromResource } from './getEntityNameFromResource';
 import { getPluralEntityName } from './getPluralEntityName';
+import kebabCaseUtil from 'kebab-case'
 
-const getHandlerModule = async ({ handlerRoot, resource, method, plural }: GetHandlerModuleParams) => {
+const getHandlerModule = async ({ handlerRoot = 'app', resource, method, plural }: GetHandlerModuleParams) => {
   const entityName = getEntityNameFromResource(resource);
   const folder = method === QueryMethod.Get ? 'queries' : 'mutations';
   return import(
-    `app/${handlerRoot.length ? `${handlerRoot}/` : ''}${resource}/${folder}/${method}${
+    `${handlerRoot.length ? `${handlerRoot}/` : ''}${resource}/${folder}/${method}${
       plural ? getPluralEntityName(entityName) : entityName
     }`
   );
@@ -18,10 +19,15 @@ export const getHandler = async ({
   resource,
   method = QueryMethod.Get,
   plural = false,
+  kebabCase,
   invoke,
 }: GetHandlerParams) => {
   if (!isPlural(resource)) {
     throw new Error(`Resource '${resource}' MUST be plural!`);
+  }
+
+  if (kebabCase) {
+    resource = kebabCaseUtil(resource)
   }
 
   const handlerModule = await getHandlerModule({ handlerRoot, resource, method, plural });
